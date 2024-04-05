d29195714681314047c8c8f53cf2a1d09051b944
MaggotOrchard <72052130+MaggotOrchard@users.noreply.github.com>
2024-04-05 09:07:31 -0700

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,13 @@
+store = "Nick's Pizza Shop"
+
+#back slashes can be used to indicate that a quotation, single or double, is not to be part of a string
+
+store = 'Nick\'s Pizza Shop, the "best" there is'
+
+print (store)
+
+# Put the name of a movie you like into a variable called movie
+
+movie = "Star Wars"
+
+print(movie)
